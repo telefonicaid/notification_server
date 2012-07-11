19bb67511d7dd9f33aaf764fba91ac1416e2d3fc
Guillermo López <willyaranda@gmail.com>
2012-07-11 18:57:03 +0200

    Try to get verification working

--- test/scripts/send_notification.sh
+++ test/scripts/send_notification.sh
@@ -1,1 +1,1 @@
-curl -d "{ \"id\": 1234, \"message\": \"$2\", \"signature\": \"SHA256_RSA Signature\", \"ttl\": 0, \"timestamp\": \"SINCE_EPOCH_TIME\", \"priority\": 1 }" http://localhost:8081/notify/$1
+curl -d "{ \"id\": 1234, \"message\": \"$2\", \"signature\": \"$3\", \"ttl\": 0, \"timestamp\": \"SINCE_EPOCH_TIME\", \"priority\": 1 }" http://localhost:8081/notify/$1
