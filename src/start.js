efe538a04f000da569a04c2b89f2ae18b2513e7a
Guillermo López <willyaranda@gmail.com>
2012-08-17 14:42:42 +0200

    Adding license header and copyright

--- src/start.js
+++ src/start.js
@@ -8,6 +8,25 @@
 var forever = require('forever-monitor');
 var starts = require("./config.js").servers;
 
+// Show license
+console.log(
+"    PUSH Notification Server \n\
+    Copyright (C) 2012  Telefonica PDI \n\
+ \n\
+    This program is free software: you can redistribute it and/or modify \n\
+    it under the terms of the GNU Affero General Public License as published by \n\
+    the Free Software Foundation, either version 3 of the License, or \n\
+    (at your option) any later version. \n\
+ \n\
+    This program is distributed in the hope that it will be useful, \n\
+    but WITHOUT ANY WARRANTY; without even the implied warranty of \n\
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n\
+    GNU Affero General Public License for more details. \n\
+ \n\
+    You should have received a copy of the GNU Affero General Public License \n\
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. \n\
+\n\n\n\n");
+
 //Fill what server should be started
 var childs = [];
 (function fillChilds() {
