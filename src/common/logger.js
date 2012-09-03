227073fef92ea8b32b9256acd11dd66b240929db
Guillermo López <willyaranda@gmail.com>
2012-09-03 12:04:24 +0200

    Fix log flags

--- src/common/logger.js
+++ src/common/logger.js
@@ -17,7 +17,7 @@ function logger() {
 logger.prototype = {
   init: function (logfile, appname, consoleOutput) {
     // use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
-    this.logfile = fs.createWriteStream(logfile, { flags: 'a', encoding: null, mode: 666 });
+    this.logfile = fs.createWriteStream(logfile, { flags: 'a', encoding: null, mode: 0666 });
     this.appname = appname;
     this.consoleOutput = consoleOutput;
     this.info("---------8<---------8<---------8<---------8<---------8<---------8<---------8<---------8<---------8<---------");
