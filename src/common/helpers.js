dc4a39096c0e24d55b543f1f6611df7c12388fb6
Fernando Rodriguez Sela <frsela@tid.es>
2012-09-06 08:57:38 +0200

    Corrected bug with the connection list on datamanager: The connection wasn't unique identified

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,7 +1,17 @@
 var publicBaseURL = require('../config.js').NS_AS.publicBaseURL;
 
+/**
+ * Gets the public notification URL for the given watoken
+ */
 function getNotificationURL(watoken) {
 	return publicBaseURL + "/notify/" + watoken;
 }
-
 exports.getNotificationURL = getNotificationURL;
+
+/**
+ * Gets a unique connection ID from a connection object
+ */
+function getConnectionId(connection) {
+	return connection.socket.remoteAddress + ":" + connection.socket.remotePort;
+}
+exports.getConnectionId = getConnectionId;
