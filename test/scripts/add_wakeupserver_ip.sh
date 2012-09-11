c0a3cc7d281b441a67d8d64fd0114aa37cf49174
Fernando Rodriguez Sela <frsela@tid.es>
2012-09-11 16:22:38 +0200

    Script to import all MCC and MNC into MongoDB. Data from http://www.itu.int/pub/T-SP-E.212B-2011

---
+++ test/scripts/add_wakeupserver_ip.sh
@@ -0,0 +1,3 @@
+echo "Adding a new WakeUp server IP address to the MongoDB"
+mongo push_notification_server --quiet --eval "db.operators.update( { _id: '$1-$2' }, { \$addToSet: { wakeup: '$3' } } )"
+echo "Done !"
