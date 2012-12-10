6686b226970f94b70f23da95554231fe21d59b91
Guillermo López Leal <willyaranda@gmail.com>
2012-12-10 14:51:19 +0100

    Update src/common/helpers.js

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,45 +1,38 @@
 /**
- * PUSH Notification server
- * (c) Telefonica Digital, 2012 - All rights reserved
- * License: GNU Affero V3 (see LICENSE file)
+ * PUSH Notification server V 0.3
+ * (c) Telefonica Digital, 2012 - All rights reserver
  * Fernando Rodríguez Sela <frsela@tid.es>
  * Guillermo Lopez Leal <gll@tid.es>
  */
 
-var publicBaseURL = require('../config.js').consts.publicBaseURL,
-    uuid = require("node-uuid"),
-    crypto = require("../common/cryptography.js"),
-    exec = require('child_process').exec;
+var publicBaseURL = require('../config.js').NS_AS.publicBaseURL;
+var uuid = require("node-uuid");
+var crypto = require("../common/cryptography.js");
 
 /**
  * Gets the public notification URL for the given apptoken
  */
 function getNotificationURL(apptoken) {
-  return publicBaseURL + "/notify/" + apptoken;
+    return publicBaseURL + "/notify/" + apptoken;
 }
 exports.getNotificationURL = getNotificationURL;
+
+/**
+ * Gets a unique connection ID from a connection object
+ */
+function getConnectionId(connection) {
+  if( typeof connection.__uniqueid == "undefined" ) {
+    connection.__uniqueid = uuid.v1();
+  }
+
+  return connection.__uniqueid;
+}
+exports.getConnectionId = getConnectionId;
 
 function getAppToken(watoken, pbkbase64) {
   return crypto.hashSHA256(watoken + pbkbase64);
 }
 exports.getAppToken = getAppToken;
-
-function padNumber(number,len) {
-  var str = '' + number;
-  while (str.length < len) {
-      str = '0' + str;
-  }
-  return str;
-}
-exports.padNumber = padNumber;
-
-function checkCallback(callback) {
-  if (typeof callback !== 'function') {
-    callback = function() {};
-  }
-  return callback;
-}
-exports.checkCallback = checkCallback;
 
 function getMaxFileDescriptors(cb) {
   exec("ulimit -n", function(error,stdout,stderr) {
