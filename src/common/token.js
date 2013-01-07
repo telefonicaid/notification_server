814c3eabb9733b8c025852f359c8e74b1a7c9451
Guillermo López Leal <willyaranda@gmail.com>
2013-01-07 03:06:33 +0100

    Delete padding workaround on token.js
    Fixed upstream some time ago on commit https://github.com/joyent/node/commit/eaf13431003b92521e3277dd7fa5dd223407052f

--- src/common/token.js
+++ src/common/token.js
@@ -20,20 +20,6 @@ token.prototype = {
     // SerialNumber + TimeStamp + NotificationServer_Id + CRC -> RAWToken
     var rawToken = this.serialNumber++ + "#" + Date.now() + "#" + process.serverId + "_" + uuid.v1();
 
-    //////////////////////////////////////////////////////////////////////////////////////
-    // Due to the Node.JS Crypto library decission (ignore padding) we should add it:
-    // @see https://github.com/joyent/node/blob/master/src/node_crypto.cc#L2156
-    /*
-     * // local decrypt final without strict padding check
-     * // to work with php mcrypt
-     * // see http://www.mail-archive.com/openssl-dev@openssl.org/msg19927.html
-     * int local_EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx,
-     *                         unsigned char *out,
-     *                         int *outl) {
-     */
-    while( (rawToken.length - 32) % 16 > 0 ) rawToken+="#";    // 32 == MD5 length
-    //////////////////////////////////////////////////////////////////////////////////////
-
     // CRC
     rawToken += "@" + crypto.hashMD5(rawToken);
 
