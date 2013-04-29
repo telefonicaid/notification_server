65f07ccae96e8f2b52897989df7d1b22dcef8f9d
Fernando Rodríguez Sela <frsela@tid.es>
2013-04-29 12:40:53 +0200

    Added support to log traces with ID and documentation

---
+++ src/common/logtraces.js
@@ -0,0 +1,17 @@
+/**
+ * PUSH Notification server
+ * (c) Telefonica Digital, 2013 - All rights reserved
+ * License: GNU Affero V3 (see LICENSE file)
+ * Fernando Rodríguez Sela <frsela@tid.es>
+ * Guillermo Lopez Leal <gll@tid.es>
+ */
+
+/**
+ * Log traces IDs format (per digit):
+ *  1: Log level (1=NOTIFY,)
+ *  2: Server (0=COMMON, 1=AS, 2=MONITOR, 3=UAWS, 4=UAUDP, 5=WakeUp)
+ *  3 & 4: Message ID
+ */
+
+exports.logtraces = {
+}
