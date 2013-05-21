4fe82c96549d21672763a45f34f3399223f9d7ba
Carmen Jimenez <macajc@gmail.com>
2013-05-21 13:06:57 +0200

    include superreview comments

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -75,7 +75,8 @@ exports.isVersion = isVersion;
 
 
 function getCaChannel() {
-    
+   
+  var path = require('path');
   var caDir = require('../config.js').consts.caDir;
   var fs = require('fs');
 
@@ -87,7 +88,7 @@ function getCaChannel() {
   var ca = [];
 
   for (i = 0, len = files.length; i < len; i++) {
-    ca.push(fs.readFileSync(caDir + "/" + files[i]));
+    ca.push(fs.readFileSync(caDir + path.sep + files[i]));
   }  
   return ca;
 }
