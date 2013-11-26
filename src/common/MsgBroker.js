telefonicaid/notification_server

commit 09b124d4d310eb93b6bf72dc51b6cef9d8065ef6

OWD-30769.  Rabbitmq increases memory usage

src/common/MsgBroker.js
@@ -82,16 +82,14 @@ MsgBroker.prototype.subscribe = function (queueName, args, broker, callback) {
  */
 MsgBroker.prototype.push = function (queueName, obj) {
   Log.debug('msgbroker::push --> Sending to the queue ' + queueName + ' the package:', obj);
-  //Send to one of the connections that is connected to a queue
-  //TODO: send randomly , not to the first open connection (which is the easiest 'algorithm')
-  var sent = false;
-  this.queues.forEach(function (connection) {
-    var exchange = connection.exchange(queueName + '-fanout', {type: 'fanout'});
-    if (connection && !sent) {
-      exchange.publish(queueName, obj, { contentType: 'application/json', deliveryMode: 1 });
-      sent = true;
+  var i = this.queues.length;
+  while(!this.exchange && i > 0) {
+    if (this.queues[i-1]) {
+      this.exchange = this.queues[i-1].exchange(queueName + '-fanout', {type: 'fanout'});
     }
-  });
+    i--;
+  }
+  this.exchange.publish(queueName, obj, { contentType: 'application/json', deliveryMode: 1 });
 };
 
 MsgBroker.prototype.createConnection = function (queuesConf) {
@@ -147,6 +145,7 @@ MsgBroker.prototype.createConnection = function (queuesConf) {
     });
     conn.state = QUEUE_ERROR;
     self.emit('queuedisconnected');
+    self.exchange = undefined;
   }));
 
   conn.on('heartbeat', (function () {

commit 51dde674654061a0412c1e1ec9e32eea24a9c982

OWD-30771.  "onNewMessage Not enough data!" errors on Websockets. Regression from commit 09b124d4d310eb93b6bf72dc51b6cef9d8065ef6

src/common/MsgBroker.js
@@ -25,6 +25,7 @@ function MsgBroker() {
   events.EventEmitter.call(this);
   this.queues = [];
   this.conns = [];
+  this.exchangeNames = {};
   this.controlledClose = false;
 }
 
@@ -83,13 +84,19 @@ MsgBroker.prototype.subscribe = function (queueName, args, broker, callback) {
 MsgBroker.prototype.push = function (queueName, obj) {
   Log.debug('msgbroker::push --> Sending to the queue ' + queueName + ' the package:', obj);
   var i = this.queues.length;
-  while(!this.exchange && i > 0) {
+  var exchangeName = queueName + '-fanout';
+  /**
+   * We need to create an exchange for each of the queues where we need
+   * to push. Remember that a SINGLE exchange is not possible since we can push
+   * to two different queues (UDP and WS1, for example)
+   */
+  while(!this.exchangeNames[exchangeName] && i > 0) {
     if (this.queues[i-1]) {
-      this.exchange = this.queues[i-1].exchange(queueName + '-fanout', {type: 'fanout'});
+      this.exchangeNames[exchangeName] = this.queues[i-1].exchange(queueName + '-fanout', {type: 'fanout'});
     }
     i--;
   }
-  this.exchange.publish(queueName, obj, { contentType: 'application/json', deliveryMode: 1 });
+  this.exchangeNames[exchangeName].publish(queueName, obj, { contentType: 'application/json', deliveryMode: 1 });
 };
 
 MsgBroker.prototype.createConnection = function (queuesConf) {